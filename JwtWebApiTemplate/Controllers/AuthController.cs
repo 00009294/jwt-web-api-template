f7d0309b707259bb2a0086fa63bb5448e42fdf6a
Javlonbek fayzullaev <fayzullaev.javlonbek.d@gmail.com>
2023-05-01 00:55:08 +0500

    Authorize done in weather controller

--- JwtWebApiTemplate/Controllers/AuthController.cs
+++ JwtWebApiTemplate/Controllers/AuthController.cs
@@ -47,7 +47,7 @@ namespace JwtWebApiTemplate.Controllers
             List<Claim> claims = new List<Claim>()
             {
                 new Claim(ClaimTypes.Name, user.Username),
-                new Claim(ClaimTypes.Hash.ToString(), user.PasswordHash.ToString())
+               
             };
             var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
             _configuration.GetSection("AppSettings:Token").Value));
