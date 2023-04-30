7c495053348bc54374d338a4ffcf462727cdffa5
Javlonbek fayzullaev <fayzullaev.javlonbek.d@gmail.com>
2023-04-30 21:20:49 +0500

    register/login done

---
+++ JwtWebApiTemplate/Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace JwtWebApiTemplate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+         public static User user= new User();
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            CreatePasswordHash(request.Password, out byte[] PasswordHash, out byte[] PasswordSalt);
+            user.Username= request.Username;
+            user.PasswordHash= PasswordHash;
+            user.PasswordSalt= PasswordSalt;
+            return Ok(user);
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+            if(request.Username != user.Username)
+            {
+                return BadRequest("User not found");
+            }
+            if(VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt)) 
+            {
+                return BadRequest("Wrong password");
+            }
+            return Ok("My crazy Token");
+        }
+        private void CreatePasswordHash(string password, out byte[] Hash, out byte[] Salt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                Hash = hmac.Key;
+                Salt = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+        private bool VerifyPasswordHash(string password, byte[] hash, byte[] salt)
+        {
+            using(var hmac = new HMACSHA512(salt)) 
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(hash);
+            }
+        }
+    }
+}
